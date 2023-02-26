547833cb308acf8ac351b9bffb351372fa8f1707
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-02-25 23:38:33 -0600

    Refactorizando codigo

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,21 +1,11 @@
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+import { addHours } from 'date-fns'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import enUS from 'date-fns/locale/en-US'
 
+import { getMessagesCalendar, localizer } from '../../helpers'
 import { Navbar } from '../'
 
-const locales = {
-  'en-US': enUS,
-}
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
 
 const events = [{
   title: 'Cumpleaños del jefe',
@@ -27,18 +17,26 @@ const events = [{
 
 export const CalendarPage = () => {
   
+  const eventsCalendar = ( event, start, end, isSelected ) => {
+    console.log({event, start, end, isSelected});
+
+  }
   
   return (
     <>
       <Navbar/>
 
       <Calendar
+      culture='es'
       localizer={localizer}
       events={events}
       startAccessor="start"
       endAccessor="end"
       style={{ height: 'calc( 100vh - 80px)' }}
+      messages={getMessagesCalendar()}
+      eventPropGetter={eventsCalendar}
     />
+      
     </>
   )
 }
---
+++ src/helpers/calendarMessagesEs.js
@@ -0,0 +1,20 @@
+
+export const getMessagesCalendar = () => {
+
+  return {
+    allDay: 'Todo el día',
+    previous: '<',
+    next: '>',
+    today: 'Hoy',
+    month: 'Mes',
+    week: 'Semana',
+    day: 'Día',
+    agenda: 'Agenda',
+    date: 'Fecha',
+    time: 'Hora',
+    event: 'Evento',
+    noEventsInRange: 'No hay eventos en este rango',
+    showMore: total => `+ Ver más (${total})`
+  }
+
+}
